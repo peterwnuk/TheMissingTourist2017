c3ef54a3914dfa3f0f68f233b543378f3a42006b
peterwnuk <piotr.i.wnuk@gmail.com>
2017-02-28 02:21:40 -0500

    Add files via upload

---
+++ nav.php
@@ -0,0 +1,18 @@
+				<nav>
+		<div class="inner-nav">
+							<ul class="">
+							<a href="#" class="home-link"><li class="transition">Home</li></a>
+							<a href="#" class="synopsis-link"><li class="transition">Synopsis</li></a>
+							<li class="transition display-none-sm">Crew</li>
+							<li class="transition display-none-sm">Contact</li>
+							<li class="transition display-none-sm">Screenings</li>
+							<li class="transition display-none-sm">Reviews</li>
+							<li class="transition display-none-sm">Trailer</li>
+							
+						</ul>
+							<div class="hamburger-wrapper"><button class="c-hamburger c-hamburger--htx">
+		  						<span></span>
+							</button>
+							</div>
+						</div>
+					</nav>
---
+++ synopsis.php
@@ -0,0 +1,90 @@
+ <div id="synopsis-page" class="synopsis-to-top">
+ 	<div class="container">
+ 	<div class="header-image relative">
+ 		<div class="overlay">
+
+ 			<?php include 'nav.php';?>
+            <div class="header-title vertical-align">
+                 <h1 class="uppercase">Synopsis</h1>
+               	<h3 class="font-white center-text">- A Japanese tourist goes missing in Yellowknife and leaves a mystery in her wake -</h3>
+            </div>
+        </div>
+
+     </div>
+     <a href="#" class="home-link back-button"><i class="vertical-align transition font-white fa fa-arrow-left fa-1x"></i></a>
+</div>
+<div class="container white-background">
+	<div class="long-synopsis">
+		<h4 class="grey-font">On October 17, 2014, Atsumi Yoshikubo arrived in Yellowknife, Canada, from Japan and 
+			checked into the Explorer Hotel. Over the next few days, she visited a gift shop and a tourist information 
+			centre where she made inquiries about viewing the aurora borealis – the main attraction for Japanese 
+			tourists visiting the Northwest Territories. Five days after she arrived, Atsumi went walking along the 
+			main road heading out of the city and was never seen again.
+		</h4>
+		<h4 class="grey-font">THE MISSING TOURIST tells the story of Atsumi Yoshikubo’s mysterious disappearance and 
+			the urgent missing person investigation that followed, with the RCMP ultimately – and controversially – 
+			concluding that the 45-year-old doctor “arrived in Yellowknife with a plan to go into the wilderness alone 
+			and become a missing person.” 
+
+
+		</h4>
+		<h4 class="grey-font">When the RCMP made their surprising announcement just nine days into the search, few in 
+			Yellowknife could make sense of it. For a tourist to go missing was almost unheard of. Over the course of 
+			the missing person investigation, several facts emerged that made Atsumi’s case particularly unusual. While 
+			thousands of Japanese tourists visit Yellowknife every year, they usually travel in groups, and typically on 
+			pre-arranged tours. Atsumi came alone. She also came during the off-season, when higher than normal cloud 
+			coverage makes it difficult to see the aurora. In fact, most tour companies do not even operate in October. 
+			Atsumi’s “planned disappearance” was also unusual because she had left belongings in her hotel room – 
+			including souvenirs from a local gift shop – which suggested she was going to return. 
+
+		</h4>
+		<h4 class="grey-font">Shortly after the search was called off, the Japanese media – where the story had been 
+			leading TV newscasts – reported that Atsumi had sent a note to a friend before she left Japan that 
+			suggested she might end her life in Yellowknife. But even the source of the report, Atsumi’s brother, 
+			questioned the veracity of the note, as well as its consistency with the facts of the case. His skepticism 
+			was fueled by Japan’s Ministry of Foreign Affairs, who wouldn’t share with him any further details from the 
+			note, nor would they reveal its recipient. 
+
+		</h4>
+		<h4 class="grey-font">The community of Yellowknife was devastated by Atsumi’s disappearance, with many 
+			remaining unconvinced by the notion that she had done so intentionally. People are still haunted by her 
+			memory, which continues to raise questions that can never be fully answered.  
+
+		</h4>
+		<h4 class="grey-font">THE MISSING TOURIST sheds new light on the investigation and goes beyond the events 
+			covered in the news to find out what really happened to Atsumi Yoshikubo. Tracing Atsumi’s journey from 
+			a small prefecture in southern Japan to the wilderness of the Northwest Territories – including interviews 
+			with family, friends and colleagues in Japan as well as media, police officials and members of the 
+			community in Yellowknife who were among the last to see her – the film attempts to bring a sense of 
+			closure to one of the most unsettling mysteries of the Canadian North in recent memory.
+
+  
+
+		</h4>
+	</div>
+	<a href="#" class="scroll-up-button transition scroll-up-button-grey"><i class="font-white fa fa-angle-up"></i></a>
+</div>
+<div class="container">
+	<footer id="">
+            			<div class="one-third-block">
+            			<div class="logo"></div>
+            			<p class="font-white copyright">© 2017 Copyright BigCedar Films. All rights reserved</p>
+            			</div>
+            			<div class="one-third-block">
+		            	
+		            	</div>
+		            	<div class="one-third-block">
+		            	<div class="social-icons vertical-align">
+		            			<ul>
+		            				<a href="www.Facebook.com/MissingTouristDoc"><li class="transition">
+		            					<i class="font-white fa fa-facebook"></i>
+		            				</li></a>
+		            				<a href="#"><li class="transition"><i class="font-white fa fa-instagram"></i></li></a>
+		            				<a href="#"><li class="transition"><i class="font-white fa fa-twitter"></i></li></a>
+		            			</ul>
+		            		</div>
+		            		</div>
+        				</footer>
+</div>
+</div>
+
