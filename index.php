ee5f830ccff3f0c98a6add94ac29f129b6f67ca1
peterwnuk <piotr.i.wnuk@gmail.com>
2017-02-28 02:19:48 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,127 @@
+<!doctype html>
+<html lang="en" class="no-js" prefix="og: http://ogp.me/ns#">
+<head>
+	<meta property="og:title" content="Web Design" />
+  	<meta property="og:description" content="Responsive, Custom, Functional Websites" />
+  	<meta property="og:url" content="http://atomboxdesign.com/index.html" />
+  	<meta property="og:image" content="http://atomboxdesign.com/img/website_image.jpg" />
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	
+	<!-- Google Fonts style -->
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Oswald:400,300,700' rel='stylesheet' type='text/css'>
+	
+	<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css"> <!-- Font Awesome -->
+	<link rel="stylesheet" href="css/reset.css"> <!-- CSS reset -->
+	<link rel="stylesheet" href="css/styles.css"> <!-- Resource style -->
+	
+	<script src="js/modernizr.js"></script> <!-- Modernizr -->
+	<script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js"></script>
+	<script>
+	    WebFont.load( { 
+	        custom: {
+	            families: [ '', '' ]
+	        },
+	        active: function() {
+	            //render your page
+	        },
+	    } );
+	</script>
+	
+	<link rel="shortcut icon" href="../img/logo_favicon.png" type="image/x-icon">
+	<title>Missing Tourist</title>
+</head>
+<body>
+	
+	<div id="main-page" class="to-top">
+		
+		<div id="fixed-block">
+			
+				<div class="half-block relative">
+					<div class="overlay overlay-dark overflow-scroll">
+
+				<div class="short-synopsis">
+				<h3 class="font-white">A Japanese tourist goes 
+                			missing in Yellowknife leaving behind luggage, souvenirs – and a mystery to be solved.</h3>
+
+               	<h4 class="font-white">On October 17, 2014, Atsumi Yoshikubo arrived in Yellowknife, Canada, from Japan 
+               		and checked into the Explorer Hotel. Over the next few days, she visited a gift shop and a tourist 
+               		information centre where she made inquiries about viewing the aurora borealis – the main attraction 
+               		for Japanese tourists visiting the Northwest Territories. Five days after she arrived, Atsumi went 
+               		walking along the main road heading out of the city and was never seen again.</h4>
+               	<h4 class="font-white">THE MISSING TOURIST tells the story of Atsumi Yoshikubo’s mysterious disappearance 
+               		and the urgent missing person investigation that followed, with the RCMP ultimately – and controversially 
+               		– concluding that the 45-year-old doctor “arrived in Yellowknife with a plan to go into the wilderness 
+               		alone and become a missing person.” </h4>
+               	<h4 class="font-white">Tracing Atsumi’s journey from a small prefecture in southern Japan to the wilderness 
+               		of the Northwest Territories, the documentary sheds new light on the investigation and goes beyond the 
+               		events covered in the news to find out what really happened to Atsumi Yoshikubo. With interviews with 
+               		family, friends and colleagues in Japan as well as media, police officials and members of the community 
+               		in Yellowknife who were among the last to see her, THE MISSING TOURIST attempts to bring a sense of closure 
+               		to one of the most unsettling mysteries of the Canadian North in recent memory.
+
+ </h4>
+ <a class="transition button horizontal-center synopsis-link"><i class="fa fa-cube"></i> Full Synopsis</a>
+			</div>
+
+
+			<div class="fixed-logo">
+				<div class="logo"></div>
+            			<p class="font-white copyright">© 2017 Copyright BigCedar Films. All rights reserved</p>
+            	</div>
+			</div>
+			</div>
+			
+			<a href="#" class="scroll-up-button transition"><i class="font-white fa fa-angle-up"></i></a>
+		</div>
+		<div id="scroll-block">
+			<div id="home-content">
+				<div class="overlay">
+					<?php include 'nav.php';?>
+            		<div class="vertical-align maincontent">
+                 		<h1 class="uppercase">The Missing Tourist</h1>
+                 		<h2 class="center-text uppercase">Premiering MARCH 2, 9pm | <a href="http://www.cbc.ca/firsthand/episodes/the-missing-tourist" target="_blank"><span class="font-red">CBC</span></a> Firsthand</h2>
+                		
+                		<a class="transition trailer"><i class="fa fa-play"></i></a>
+            		</div>
+            		
+            		<div id="home-bottom-section">
+            			<div class="one-third-block">
+            			<div class="logo"></div>
+            			<p class="font-white copyright">© 2017 Copyright BigCedar Films. All rights reserved</p>
+            			</div>
+            			<div class="one-third-block">
+		            	<a href="#"><i class="vertical-align font-white fa fa-angle-down fa-3x animate-arrow"></i></a>
+		            	</div>
+		            	<div class="one-third-block">
+		            	<div class="social-icons vertical-align">
+		            			<ul>
+		            				<a href="www.Facebook.com/MissingTouristDoc"><li class="transition">
+		            					<i class="font-white fa fa-facebook"></i>
+		            				</li></a>
+		            				<a href="#"><li class="transition"><i class="font-white fa fa-instagram"></i></li></a>
+		            				<a href="#"><li class="transition"><i class="font-white fa fa-twitter"></i></li></a>
+		            			</ul>
+		            		</div>
+		            		</div>
+        				</div>
+            	</div>
+            
+            </div>
+            
+            
+            <div id="transparent-block"></div>
+        </div>
+            
+        </div>
+             
+       <?php include 'synopsis.php';?>
+
+<!-- Jquery -->
+<script src="js/jquery-2.1.1.js"></script>
+<script src="js/main.js"></script>
+</body>
+
+</html>
